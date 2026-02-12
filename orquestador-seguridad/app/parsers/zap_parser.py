de89cb6abccf06e73c373dad9da4b3b0743cb94e
CristianK25 <krahulikcristian@gmail.com>
2026-02-12 17:42:44 -0300

    feat: OWASP ZAP parser base

---
+++ orquestador-seguridad/app/parsers/zap_parser.py
@@ -0,0 +1,39 @@
+import json
+
+def zap_parser(ruta_archivo):
+    #Abrir el archivo JSON con los datos que trae OWASP ZAP
+    with open(ruta_archivo,"r") as zap:
+        #Cargas la info del zap en la variable data
+        data = json.load(zap)
+
+    #Alertas que trae OWASP ZAP
+    alertas_sin_parsear = data["site"][0]["alerts"]
+
+    #Datos Principales de OWASP ZAP
+    datos_site = {
+        "herramienta":"ZAP",
+        "fecha_de_generacion": data["@generated"],
+        "url_atacada": data["site"][0]["@name"],
+        "host": data["site"][0]["@host"],
+        "puerto": data["site"][0]["@port"],
+        "ssl": data["site"][0]["@ssl"]
+    }
+    #Limpiar la informacion irrelevante del diccionario "data"
+    alertas_parseado = [
+        {
+            "vulnerabilidad": item["alert"],
+            "url": [i["uri"] for i in item["instances"]],
+            "severidad": item["riskdesc"],
+            "solucion": item["solution"]
+        }
+        for item in alertas_sin_parsear
+    ]
+
+    #Agregar la lista de datos parseados a los datos del site
+    datos_site["alertas"] = alertas_parseado
+
+    #Retornar los datos limpios
+    return datos_site
+
+
+print(zap_parser("orquestador-seguridad/app/samples/zap_sample.json"))
