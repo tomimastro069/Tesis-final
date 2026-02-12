df0d72cc781ac99aee9f949c521a4559b5e8a115
CristianK25 <krahulikcristian@gmail.com>
2026-02-12 16:04:43 -0300

    refactor(ffuf-parser): añado excepciones

--- orquestador-seguridad/app/parsers/ffuf_parser.py
+++ orquestador-seguridad/app/parsers/ffuf_parser.py
@@ -3,23 +3,29 @@ import json
 ruta_archivo_ejemplo = "orquestador-seguridad/app/samples/ffuf_sample.json"
 
 def ffuf_parser(ruta_archivo):
-    #Abro el archivo JSON 
-    with open(ruta_archivo,"r") as f:
-        #Cargo el archivo JSON en la variable data
-        data = json.load(f)
+    try:
+        #Abro el archivo JSON 
+        with open(ruta_archivo,"r") as f:
+            #Cargo el archivo JSON en la variable data
+            data = json.load(f)
 
-    #Limpiar lista segun los resultados que tienen status = 200
-    lista_limpia = [
-        {
-            "Herramienta":"ffuf",
-            "input":item["input"],
-            "url":item["url"],
-            "host":item["host"],
-            "status":item["status"]
-        }
-        for item in data["results"] 
-        if item["status"] == 200
-    ]
+        #Limpiar lista segun los resultados que tienen status = 200
+        lista_limpia = [
+            {
+                "Herramienta":"ffuf",
+                "input":item["input"],
+                "url":item["url"],
+                "host":item["host"],
+                "status":item["status"]
+            }
+            for item in data["results"] 
+            if item["status"] == 200
+        ]
 
-    #Retornar la lista limpia
-    return lista_limpia
+        #Retornar la lista limpia
+        return lista_limpia
+
+    except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
+        print(f"Error al parsear el archivo ffuf: {e}")
+        return []
+
